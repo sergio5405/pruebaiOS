97fe6cb1df0bf03f61b95a395a0a04520f0f3f5d
Sergio Hernandez <sergio54058@gmail.com>
2018-08-09 14:20:20 -0500

    Primer avance app

---
+++ PruebaNextia/LoginVC.swift
@@ -0,0 +1,35 @@
+//
+//  LoginVC.swift
+//  PruebaNextia
+//
+//  Created by Sergio Hernández Méndez on 8/9/18.
+//  Copyright © 2018 Sergio Hernández Méndez. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PruebaNextia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaNextia
+//
+//  Created by Sergio Hernández Méndez on 8/9/18.
+//  Copyright © 2018 Sergio Hernández Méndez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
